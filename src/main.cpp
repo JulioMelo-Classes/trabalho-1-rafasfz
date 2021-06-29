dd6817b5b4533199661d640dbae5d49a4950a6c0
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-06-29 13:59:46 -0300

    Pegando os dados de input

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,18 @@ int main(int argc, char *argv[]) {
   std::string file_name = argv[1];
 
   std::cout << ">>> Lendo arquivo de apostas [" << file_name << "], por favor aguarde.." << std::endl;
-
   std::cout << "--------------------------------------------------------------------" << std::endl;
 
   bool input_is_valid = validate_input(file_name);
 
   if(!input_is_valid) {
     invalid_input();
-
     return 1;
   }
+
+  double ic = get_ic(file_name);
+  unsigned int nr = get_nr(file_name);
+  std::vector<int> spots = get_spots(file_name);
   
 
   return 0;
