8bf1c9cd3859c0abf662e2e6b48a8e0d6264050d
Rafael Silva Freire <rafaelsilvafreire2@gamil.com>
2021-07-02 19:08:38 -0300

    Removido comentarios nas implementaçoes

--- include/KenoBet.h
+++ include/KenoBet.h
@@ -35,34 +35,34 @@ public:
     KenoBet() : m_wage(0){/* empty */};
 
     /*! Adds a number to the spots only if the number is not already there.
-              @param spot_ The number we wish to include in the bet.
-              @return T if number chosen is successfully inserted; F otherwise. */
+    @param spot_ The number we wish to include in the bet.
+    @return T if number chosen is successfully inserted; F otherwise. */
     bool add_number(number_type spot_);
 
     /*! Sets the amount of money the player is betting.
-              @param wage_ The wage.
-              @return True if we have a wage above zero; false otherwise. */
+    @param wage_ The wage.
+    @return True if we have a wage above zero; false otherwise. */
     bool set_wage(cash_type wage_);
 
     //! Resets a bet to an empty state.
     void reset(void);
 
     /*! Retrieves the player's wage on this bet.
-              @return The wage value. */
+    @return The wage value. */
     cash_type get_wage(void);
 
     /*! Returns to the current number of spots in the player's bet.
-              @return Number of spots present in the bet. */
+    @return Number of spots present in the bet. */
     size_t size(void);
 
     /*! Determine how many spots match the hits passed as argument.
-              @param hits_ List of hits randomly chosen by the computer.
-              @return An vector with the list of hits. */
+    @param hits_ List of hits randomly chosen by the computer.
+    @return An vector with the list of hits. */
     set_of_numbers_type
     get_hits(const set_of_numbers_type &hits_);
 
     /*! Return a vector< spot_type > with the spots the player has picked so far.
-              @return The vector< spot_type > with the player's spots picked so far. */
+    @return The vector< spot_type > with the player's spots picked so far. */
     set_of_numbers_type get_spots(void);
 
     set_of_numbers_type get_random_hits();
--- src/KenoBet.cpp
+++ src/KenoBet.cpp
@@ -18,13 +18,10 @@ void show_hits(int spots_size)
   }
 }
 
-using number_type = unsigned short int; //<! data type for a keno hit.
-using cash_type = float;                //<! Defines the wage type in this application.
+using number_type = unsigned short int;
+using cash_type = float;               
 using set_of_numbers_type = std::vector<number_type>;
 
-/*! Adds a number to the spots only if the number is not already there.
-              @param spot_ The number we wish to include in the bet.
-              @return T if number chosen is successfully inserted; F otherwise. */
 bool KenoBet::add_number(number_type spot_)
 {
   for (number_type i : this->m_spots)
@@ -38,9 +35,6 @@ bool KenoBet::add_number(number_type spot_)
   return true;
 }
 
-/*! Sets the amount of money the player is betting.
-              @param wage_ The wage.
-              @return True if we have a wage above zero; false otherwise. */
 bool KenoBet::set_wage(cash_type wage_)
 {
   if (wage_ < 0)
@@ -51,30 +45,22 @@ bool KenoBet::set_wage(cash_type wage_)
   return true;
 }
 
-//! Resets a bet to an empty state.
 void KenoBet::reset(void)
 {
   this->m_wage = 0;
   this->m_spots.clear();
 }
 
-/*! Retrieves the player's wage on this bet.
-              @return The wage value. */
 cash_type KenoBet::get_wage(void)
 {
   return this->m_wage;
 }
 
-/*! Returns to the current number of spots in the player's bet.
-              @return Number of spots present in the bet. */
 size_t KenoBet::size(void)
 {
   return this->m_spots.size();
 }
 
-/*! Determine how many spots match the hits passed as argument.
-              @param hits_ List of hits randomly chosen by the computer.
-              @return An vector with the list of hits. */
 set_of_numbers_type
 KenoBet::get_hits(const set_of_numbers_type &hits_)
 {
@@ -93,8 +79,6 @@ KenoBet::get_hits(const set_of_numbers_type &hits_)
   return hits_matched;
 }
 
-/*! Return a vector< spot_type > with the spots the player has picked so far.
-              @return The vector< spot_type > with the player's spots picked so far. */
 set_of_numbers_type KenoBet::get_spots(void)
 {
   return this->m_spots;
@@ -135,6 +119,6 @@ cash_type KenoBet::get_total_money()
   return this->total_money;
 }
 
-set_of_numbers_type m_spots; //<! The player's bet.
-cash_type m_wage;            //<! The player's wage
+set_of_numbers_type m_spots;
+cash_type m_wage;
 cash_type total_money = 0;
